cdfe8ab30bb217669711c2ebaf6920a667453778
Ryan Venturino <ryan.venturino@gmail.com>
2021-05-30 13:05:53 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const open = document.getElementById('open');
+const modalContainer = document.getElementById('modal-container');
+const close = document.getElementById('close');
+
+open.addEventListener('click', () => {
+    modalContainer.classList.add('show');
+})
+
+close.addEventListener('click', () => {
+    modalContainer.classList.remove('show');
+})
+
+window.addEventListener('click', e => e.target == modalContainer ? modalContainer.classList.remove('show') : false)
